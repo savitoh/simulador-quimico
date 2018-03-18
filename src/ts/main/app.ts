0270c28d8439fc6b65fdf46531d25d5a2c8830cc
savitoh <savioraires10@gmail.com>
2018-03-18 01:42:56 -0300

    Add user interface and style; Rename SimulationController file and folder Controller

--- src/ts/main/app.ts
+++ src/ts/main/app.ts
@@ -1,7 +1,7 @@
-import { ControllerSimulation } from '../Controller/ControllerSimulation';
+import { ControllerSimulation } from "../controllers/SimulationController";
 
 
 let simulation: ControllerSimulation;
 let attributesReaction = { numberOfMolecules: 100000, temperatura: 1 };
-simulation = new ControllerSimulation("reversibleFirstOrderReaction", <HTMLCanvasElement> document.getElementById('main'), attributesReaction);
+simulation = new ControllerSimulation("reversibleFirstOrderReaction", <HTMLCanvasElement> document.getElementById('chart'), attributesReaction);
 simulation.loadSimulation();
