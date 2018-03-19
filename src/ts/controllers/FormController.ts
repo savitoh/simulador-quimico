0d38683d61ab66f141b7af3fda73d81c21a2a50b
savitoh <savioraires10@gmail.com>
2018-03-19 01:07:41 -0300

    add actionsin controller form

--- src/ts/controllers/FormController.ts
+++ src/ts/controllers/FormController.ts
@@ -1,18 +1,31 @@
 const selectedOption  = <HTMLSelectElement>document.getElementById("opcoes-reacoes");
-const buttonStartReaction = <HTMLElement>document.getElementById("btn-start-reaction");
-const divEnergiaAtivacaoB = <HTMLElement>document.getElementById("energia-ativacao-b").parentNode.parentNode;
-const divChart = <HTMLElement>document.getElementById("chart");
+const buttonStartReaction = <HTMLButtonElement>document.getElementById("btn-start-reaction");
+const divEnergiaAtivacaoB = <HTMLInputElement>document.getElementById("energia-ativacao-b").parentNode.parentNode;
+const divChart = <HTMLDivElement>document.getElementById("chart");
 
+const elementsIDs = ["numberOfMolecules", "temperatura", "energia-ativacao-a", "energia-ativacao-b"];
 
-function visibleDivOptionSelected() {
-  if(selectedOption.value=="reversibleFirstOrderReaction") 
+function getElemets(IDs: string[]){
+    return IDs.reduce(function(acc: any, id) {
+        const element =  <HTMLInputElement>document.getElementById(id);
+        acc[id] = element.valueAsNumber;
+        return acc;
+    },{});
+}
+
+function getValuesInputs() {
+    return getElemets(elementsIDs);
+}
+
+const visibleDivOptionSelected = () => {
+  if(selectedOption.value=="reversibleFirstOrderReaction")
       divEnergiaAtivacaoB.style.display  = "flex";
   else
       divEnergiaAtivacaoB.style.display  = "none";
-}
+};
 
-function visibleDivChart() {
-    divChart.style.display = "block";
-}
+const visibleDivChart = () => divChart.style.display = "block";
 
-export {visibleDivOptionSelected, visibleDivChart};
+const getReactionSelected = () => {return selectedOption.value};
+
+export {visibleDivOptionSelected, visibleDivChart, getReactionSelected, getValuesInputs};
--- src/ts/main/app.ts
+++ src/ts/main/app.ts
@@ -1,20 +1,23 @@
 import { SimulationController } from "../controllers/SimulationController";
 import {visibleDivOptionSelected} from "../controllers/FormController";
 import {visibleDivChart} from "../controllers/FormController";
-
+import {getReactionSelected} from "../controllers/FormController";
+import {getValuesInputs} from "../controllers/FormController";
 
 
 const selectedOption  = <HTMLSelectElement>document.getElementById("opcoes-reacoes");
 const buttonStartReaction = <HTMLElement>document.getElementById("btn-start-reaction");
+
 let simulation: SimulationController;
 let attributesReaction = { numberOfMolecules: 100000, temperatura: 1 };
 
-
 selectedOption.addEventListener("change", visibleDivOptionSelected);
 
-buttonStartReaction.addEventListener("click", function(){
+buttonStartReaction.addEventListener("click", () => {
+    console.log(getValuesInputs());
+    console.log(attributesReaction);
     visibleDivChart();
-    simulation = new SimulationController("reversibleFirstOrderReaction", <HTMLCanvasElement> document.getElementById('chart'), attributesReaction);
+    simulation = new SimulationController(getReactionSelected(), <HTMLCanvasElement> document.getElementById('chart'), getValuesInputs());
     simulation.loadSimulation();
 });
 
