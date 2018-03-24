04c018b614d68602e0fc070681339fed9855caa1
Sávio Raires de Souza <savioraires10@gmail.com>
2018-03-24 04:22:43 -0300

    Add publish script in gh-pages; Fix repeat chart show when starting reaction

--- src/ts/controllers/SimulationController.ts
+++ src/ts/controllers/SimulationController.ts
@@ -6,40 +6,42 @@ import { IReaction } from '../models/Reactions/IReaction';
 import { ReactionFactory } from '../models/Reactions/ReactionFactory';
 import { IChartController } from '../models/Chart/IChartController';
 import { ChartControllerFactory } from '../models/Chart/ChartControllerFactory';
+import { ECharts } from 'echarts';
 
 export class SimulationController {
     
     private typeOfReaction: string;
     private canvas: HTMLCanvasElement;
     private atributes: IAttributesReaction;
+    private chartOfConcentration: ECharts;
+
 
     constructor(typeOfReaction: string, canvas: HTMLCanvasElement, atributes: IAttributesReaction) {
             this.typeOfReaction = typeOfReaction;
             this.canvas = canvas;
             this.atributes = atributes;
+            this.chartOfConcentration = echart.init(this.canvas, ChartTheme);
     }
 
     public loadSimulation(): void {
-        let option = getChartOptions(this.typeOfReaction);
+
+        this.chartOfConcentration.dispose();
 
         let factoryReaction = new ReactionFactory();
-
         let reaction = factoryReaction.getReaction(this.typeOfReaction, this.atributes);
         reaction.startReaction();
 
         let concentrationOfAandB = reaction.getConcetrations();
 
-        let chartOfConcentration = echart.init(this.canvas, ChartTheme);
-        chartOfConcentration.setOption(option);
-        window.onresize = function() {
-        if(chartOfConcentration != null && chartOfConcentration != undefined){
-            chartOfConcentration.resize();
-        }
-        };
+        this.chartOfConcentration = echart.init(this.canvas, ChartTheme);
+        let option = getChartOptions(this.typeOfReaction);
+        this.chartOfConcentration.setOption(option);
+        (<any>window).onresize = () => this.chartOfConcentration.resize();
+        
 
         let chartControllerFactory = new ChartControllerFactory();
 
-        let chartController  = chartControllerFactory.getChartController(this.typeOfReaction, chartOfConcentration, concentrationOfAandB);
+        let chartController  = chartControllerFactory.getChartController(this.typeOfReaction, this.chartOfConcentration, concentrationOfAandB);
         chartController.animateChart();
     }
 
